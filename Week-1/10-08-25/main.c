77758ea15b83920cdca1c8454fc321e572ea8f4d
Roshan Dalvi <roshandalvi16@gmail.com>
2025-08-10 23:02:23 +0530

    first prgramm

---
+++ Week-1/10-08-25/main.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+
+//int argc, char const *argv[]
+int main()
+{
+    int a = 10;
+    int b = 20;
+    int sum = a+b;
+    printf("%d",sum);
+    printf("Hello Roshan");
+   
+}
