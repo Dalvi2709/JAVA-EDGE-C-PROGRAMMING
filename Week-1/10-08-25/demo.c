4233e89841a6a9358c668fcef0dd9b05634030ea
Roshan Dalvi <roshandalvi16@gmail.com>
2025-08-11 14:23:24 +0530

    \n - new line and \t - tab space

---
+++ Week-1/10-08-25/demo.c
@@ -0,0 +1,9 @@
+#include<stdio.h>  //headerfiles,Libraries-----standard input and output
+#include<conio.h>  // console input and output both gives ready made functionalities
+
+void main()
+{  
+    printf("Hello Roshan \n"); // new line or line break
+    printf("Hello\tRakesh");  // add space tab
+    
+}
