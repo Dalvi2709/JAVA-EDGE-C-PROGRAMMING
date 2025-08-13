e4505e9f528f0c751898c80b85a91147bad8ffaa
Roshan Dalvi <roshandalvi16@gmail.com>
2025-08-13 11:54:22 +0530

    loops

---
+++ Week-1/13-08-25/for_loop.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main (){
+
+    int n = 5;
+    
+    for (int i = 1; i <= n; i++) // when you know starting and ending of the loop or iteration
+    {
+        printf("Sorry\n");
+        printf("%d\n",i);
+    }
+    return 0;
+    
+}
