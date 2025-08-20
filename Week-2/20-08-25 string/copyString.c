641cf970cee889854cc910a6cae21dbc333b62ae
Roshan Dalvi <roshandalvi16@gmail.com>
2025-08-20 21:59:40 +0530

    Copy One String to Another without strcpy()

---
+++ Week-2/20-08-25 string/copyString.c
@@ -0,0 +1,19 @@
+// 5 Copy One String to Another without strcpy()
+
+#include <stdio.h>
+#include <string.h>
+
+void main()
+{
+    char str1[10] = "Roshan";
+    char str2[10];
+    int i = 0;
+
+    while (str1[i] != '\0')
+    {
+        str2[i] = str1[i];
+        i++;
+    }
+
+    printf("%s", str2);
+}
