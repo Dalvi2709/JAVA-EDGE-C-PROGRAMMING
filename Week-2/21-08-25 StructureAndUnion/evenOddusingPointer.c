798d93e923feb7862b23b50a04bfaf9eacb84e98
Roshan Dalvi <roshandalvi16@gmail.com>
2025-08-21 11:50:06 +0530

    even and odd

---
+++ Week-2/21-08-25 StructureAndUnion/evenOddusingPointer.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+    int n = 10;
+    int *p = &n;
+
+    if (*p % 2 == 0)
+    {
+        printf("\n Even");
+    }
+    else
+    {
+        printf("Odd");
+    }
+
+    return 0;
+}
