bb18b54f84091729f6a820a02037e7785d814884
Roshan Dalvi <roshandalvi16@gmail.com>
2025-09-10 10:34:03 +0530

    Even and odd from user input using ternary oprators

---
+++ Logic Building/even_odd.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+    int number;
+    printf("Enter the number\n");
+    scanf("%d",&number);
+
+    (number % 2 == 0 ) ? printf("Even number") : printf("Odd number");
+    return 0;
+}
