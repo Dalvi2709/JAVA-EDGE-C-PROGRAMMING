bd80523e3af7f7f24d8b1ee85ce7dac41d69ffa0
Roshan Dalvi <roshandalvi16@gmail.com>
2025-09-18 21:26:43 +0530

    10101 01010 10101 this like patterns

--- Logic Building/patterns/square.c
+++ Logic Building/patterns/square.c
@@ -125,7 +125,39 @@ int main()
     //     printf("\n");
     // }
 
-    
+    // 10101
+    // 01010
+    // 10101
+    // 01010
+    // 10101
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+             if (i % 2 != 0)
+             {
+                if (j % 2 != 0)
+                {
+                    printf("1");
+                }else{
+                    printf("0");
+                }
+                
+             }else{
+                if (j % 2 != 0)
+                {
+                    printf("0");
+                }else{
+                    printf("1");
+                }
+                
+             }
+             
+        }
+        printf("\n");
+        
+    }
     
     return 0;
 }   
