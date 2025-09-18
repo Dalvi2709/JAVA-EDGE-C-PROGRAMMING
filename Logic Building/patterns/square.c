a29e9fadaeb190e47b217569069398986b393433
Roshan Dalvi <roshandalvi16@gmail.com>
2025-09-18 19:58:35 +0530

    holo square pattern *

--- Logic Building/patterns/square.c
+++ Logic Building/patterns/square.c
@@ -81,4 +81,27 @@ int main()
     //     printf("\n");
     // }
 
-}
+    // *****
+    // *   *
+    // *   *
+    // *   *
+    // *****
+
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            if (i==1 || i == n || j == 1 || j == n)
+            {
+                printf("*");
+            }else 
+            {
+                printf(" ");
+            }
+        }
+            printf("\n");        
+    }
+    
+    return 0;
+}   
