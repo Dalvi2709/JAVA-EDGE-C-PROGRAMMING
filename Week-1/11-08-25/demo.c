e7d2ccd05dda0bf725f378d008b30a5e0f797185
Roshan Ramchandra Dalvi <112648015+Dalvi2709@users.noreply.github.com>
2025-11-01 21:23:03 +0530

    Fix formatting in demo.c

--- Week-1/11-08-25/demo.c
+++ Week-1/11-08-25/demo.c
@@ -3,7 +3,7 @@
 
 void main()
 {  
-    printf("Hello Roshan \n"); // new line or line break
+    printf("Hello Roshan \n"); // new line or line break 
     printf("Hello\tRakesh");  // add space tab
     
 }
