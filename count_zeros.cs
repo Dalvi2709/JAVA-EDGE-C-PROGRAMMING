850f78d6333a76490d2e81094ca51d2e9a506837
Roshan Ramchandra Dalvi <112648015+Dalvi2709@users.noreply.github.com>
2025-11-04 23:21:44 +0530

    Add program to count zeros in a number

---
+++ count_zeros.cs
@@ -0,0 +1,14 @@
+using System;
+class Program
+{
+    static void Main()
+    {
+        int n = 1050200, count = 0;
+        while (n > 0)
+        {
+            if (n % 10 == 0) count++;
+            n /= 10;
+        }
+        Console.WriteLine("Zeros: " + count);
+    }
+}
