0f53cca57b8ab08a297bcc370a63bdc56697e1d8
Roshan Ramchandra Dalvi <112648015+Dalvi2709@users.noreply.github.com>
2025-11-05 23:21:21 +0530

    Add 2D array example in C#

---
+++ 2D_Array.cs
@@ -0,0 +1,14 @@
+using System;
+class Program
+{
+    static void Main()
+    {
+        int[,] arr = { {1, 2}, {3, 4}, {5, 6} };
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 2; j++)
+                Console.Write(arr[i, j] + " ");
+            Console.WriteLine();
+        }
+    }
+}
