560e6cb2fef23fd32443a7d3253473ff4614a350
Roshan Ramchandra Dalvi <112648015+Dalvi2709@users.noreply.github.com>
2025-11-05 23:27:06 +0530

    Add matrix transposition example in C#

---
+++ Transpose_Matrix.cs
@@ -0,0 +1,14 @@
+using System;
+class Program
+{
+    static void Main()
+    {
+        int[,] arr = { {1, 2}, {3, 4} };
+        for (int i = 0; i < 2; i++)
+        {
+            for (int j = 0; j < 2; j++)
+                Console.Write(arr[j, i] + " ");
+            Console.WriteLine();
+        }
+    }
+}
